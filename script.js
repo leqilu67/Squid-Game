43dc5fb73d28b44f3980ff30849c588de19f4fa6
Leqi Lu <pototoland@gmail.com>
2017-02-08 16:49:12 -0800

    Basic gameplay done. Replay possible

--- script.js
+++ script.js
@@ -6,26 +6,28 @@ gameContainer.minSquidLength = 2;
 gameContainer.squidNum = 3;
 gameContainer.gridNum = 8;
 
+gameContainer.maxBomb = 24;
 gameContainer.chances = 24;
 
 function setup(){
 	gameContainer.squidArray = [];
     gameContainer.gridArray = [];
-	gameContainer.chancesLeft.innerHTML = gameContainer.chances;
+	gameContainer.chancesLeft.innerHTML = "Bombs used: 0";
 	
 	// bomb grid setup
-	var counter = 0;
+	var counter = gameContainer.chances;
 	var counterDiv = document.getElementById("counter");
-	for (var i = 0; i < 8; i++){
+	
+	for (var i = 0; i < 3; i++){
 		var bombContainer = document.createElement("div");
+		bombContainer.setAttribute("class", "bombContainer");
 		counterDiv.appendChild(bombContainer);
-		for (var j = 0; j < 3; j++){
+		for (var j = 0; j < 8; j++){
 			var bomb = document.createElement("div");
 			bomb.setAttribute("id", "bomb" + counter);
 			bomb.setAttribute("class", "bomb");
-			bomb.innerHTML = counter;
 			bombContainer.appendChild(bomb);
-			counter++;
+			counter--;
 		}
 	}
 	
@@ -53,7 +55,8 @@ function setup(){
 		// direction of squid: 0 = horizontal, 1 = vertical
 		squid.direction = getRandomInt(0, 1);
 		squid.length = i + gameContainer.minSquidLength;
-		squid.num = i;
+		//squid.num: 0= length 2; 1= length 3; 2= length 4;
+		squid.num = i; 
 		
 		gameContainer.squidArray.completedSquid = 0;
 		gameContainer.squidArray.push(squid);
@@ -72,7 +75,6 @@ function setup(){
 }
 
 function decideLocation(squid){
-	squid.direction = 0;
 	if (squid.direction == 0){ //horizontal
 		squid.x = getRandomInt(0, gameContainer.gridNum - 1 - squid.length);
 		squid.y = getRandomInt(0, gameContainer.gridNum - 1);
@@ -86,14 +88,22 @@ function decideLocation(squid){
 		gameContainer.squidArray[squid.num] = [];
 		gameContainer.squidArray[squid.num].numClicked = 0;
 		gameContainer.squidArray[squid.num].complete = 0;
+		gameContainer.squidArray[squid.num].direction = squid.direction;
+		gameContainer.squidArray[squid.num].startingPostion = {x: squid.x, y: squid.y};
 			
 		if (conflict(gameContainer.gridArray)){
 			gameContainer.gridArray = [];
 			decideLocation(squid);
 		} else {
+			var parts = 0;
 			for (var i = 0; i < gameContainer.gridArray.length; i++){
-				gameContainer.gridArray[i].occupied = 1;
-				gameContainer.gridArray[i].style.backgroundColor = "yellow";
+				gameContainer.gridArray[i].occupied = 1;				
+				
+				var backgroundImageURL = "Assets/squid_" + squid.num + "_" + parts + ".png";
+				parts++;
+				gameContainer.gridArray[i].backgroundImage = "url(" + backgroundImageURL + ")";
+				rotateImage(gameContainer.gridArray[i], -90);
+				
 				gameContainer.gridArray[i].belongTo = squid.num;
 				gameContainer.squidArray[squid.num].push(gameContainer.gridArray[i]);
 			}
@@ -109,13 +119,24 @@ function decideLocation(squid){
 			gameContainer.gridArray.push(grid);
 		}
 		
+		gameContainer.squidArray[squid.num] = [];
+		gameContainer.squidArray[squid.num].numClicked = 0;
+		gameContainer.squidArray[squid.num].complete = 0;
+		gameContainer.squidArray[squid.num].direction = squid.direction;
+		gameContainer.squidArray[squid.num].startingPostion = {x: squid.x, y: squid.y};
+		
 		if (conflict(gameContainer.gridArray)){
 			gameContainer.gridArray = [];
 			decideLocation(squid);
 		} else {
+			var parts = 0;
 			for (var i = 0; i < gameContainer.gridArray.length; i++){
 				gameContainer.gridArray[i].occupied = 1;
-				gameContainer.gridArray[i].style.backgroundColor = "yellow";
+				
+				var backgroundImageURL = "Assets/squid_" + squid.num + "_" + parts + ".png";
+				parts++;
+				gameContainer.gridArray[i].backgroundImage = "url(" + backgroundImageURL + ")";
+				
 				gameContainer.gridArray[i].belongTo = squid.num;
 				gameContainer.squidArray[squid.num].push(gameContainer.gridArray[i]);
 			}
@@ -137,8 +158,11 @@ function buttonClicked(e){
 	
 	if (target.clicked) return;
 	
+	document.getElementById("bomb"+gameContainer.chances).style.backgroundImage = "url('Assets/bomb_used.png')";
+
 	gameContainer.chances--;
-	gameContainer.chancesLeft.innerHTML = gameContainer.chances;
+	gameContainer.chancesLeft.innerHTML = "Bombs used: " + (gameContainer.maxBomb - gameContainer.chances);
+	
 	if (gameContainer.chances == 0){
 		// remove listeners
 		for (var i = 0; i < gameContainer.gridNum; i++){
@@ -171,6 +195,35 @@ function changeSquidSymbol(){
 	}
 }
 
+function showLocations(){
+	for (var i = 0; i < gameContainer.squidArray.length; i++){
+		var squid = gameContainer.squidArray[i];
+		if (squid.direction == 0){
+			for (var j = 0; j < squid.length; j++){
+				var backgroundImageURL = "Assets/squid_" + i + "_" + j + ".png";			
+				var grid = document.getElementById("button" + (squid.startingPostion.x + j) + squid.startingPostion.y);
+				grid.style.backgroundImage = "url(" + backgroundImageURL + ")";
+				rotateImage(grid, -90);
+			}
+			
+		} else {
+			for (var j = 0; j < squid.length; j++){
+				var backgroundImageURL = "Assets/squid_" + i + "_" + j + ".png";
+				var grid = document.getElementById("button" + squid.startingPostion.x + (squid.startingPostion.y + j));
+				grid.style.backgroundImage = "url(" + backgroundImageURL + ")";
+			}
+		}
+	}
+}
+
+function rotateImage(grid, deg){
+	grid.style.webkitTransform = "rotate(" + deg + "deg)";
+	grid.style.mozTransform = "rotate(" + deg + "deg)";
+	grid.style.msTransform = "rotate(" + deg + "deg)";
+	grid.style.OTransform = "rotate(" + deg + "deg)";
+	grid.style.transform = "rotate(" + deg + "deg)";
+}
+
 function checkEndGame(){
 	for (var i = 0; i < gameContainer.squidArray.length; i++){
 		if (gameContainer.squidArray[i].complete) continue;
@@ -186,16 +239,41 @@ function checkEndGame(){
 
 function endGame(){
 	if (gameContainer.squidArray.completedSquid == gameContainer.squidArray.length){
+		showLocations();
 		setTimeout(function(){
-			alert("YOU WON!"); 
+			if(confirm("You won! Your score is " + (gameContainer.maxBomb - gameContainer.chances) + ". Replay?")){
+				reset();
+			}
 		}, 200);
 	} else if (gameContainer.chances == 0) {
-		alert("sorry you lost");
+		showLocations();
+		setTimeout(function(){
+			if (confirm("Aww almost! Try again?")){
+				reset();
+			}
+		}, 200);
 	}
+}
+
+function reset(){
+	removeChildrenNodes(document.getElementById("counter"));
+	removeChildrenNodes(document.getElementById("gameContainer"));
+	removeChildrenNodes(document.getElementById("squidSymbol"));
+	
+	gameContainer.maxBomb = 24;
+	gameContainer.chances = 24;
+	
+	setup();
 }
 
 function getRandomInt(min, max){
 	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function removeChildrenNodes(parentNode){
+	while (parentNode.hasChildNodes()) {
+		parentNode.removeChild(parentNode.lastChild);
+	}
 }
 
 window.onload = setup();
